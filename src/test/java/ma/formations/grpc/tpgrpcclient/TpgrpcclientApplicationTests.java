2aa47f45a78d53d9e567fcd5848259a28f49a0e3
LasriWafa <ouafae.lasri@gmail.com>
2025-12-04 15:24:49 +0100

    Initial commit: gRPC client TP

---
+++ src/test/java/ma/formations/grpc/tpgrpcclient/TpgrpcclientApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.formations.grpc.tpgrpcclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpgrpcclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
